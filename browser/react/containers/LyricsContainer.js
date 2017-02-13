e26bdefe0d6dff8930935b07598a0fed3e1027f6
Susanna Huang <dearsusannahuang@gmail.com>
2017-02-13 18:17:10 -0500

    refactored PlayerContainer and LyricsContainer

--- browser/react/containers/LyricsContainer.js
+++ browser/react/containers/LyricsContainer.js
@@ -3,58 +3,113 @@ import store from '../store';
 import Lyrics from '../components/Lyrics';
 
 import {searchLyrics} from '../action-creators/lyrics';
+import {connect} from 'react-redux'
 
-export default class extends Component {
-
-  constructor() {
-
-    super();
-
-    this.state = Object.assign({
-      artistQuery: '',
-      songQuery: ''
-    }, store.getState());
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleArtistInput = this.handleArtistInput.bind(this);
-    this.handleSongInput = this.handleSongInput.bind(this);
-
+function mapStateToProps(state){
+  return {
+    state: state
   }
-
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(() => {
-      this.setState(store.getState());
-    });
-  }
-
-  handleArtistInput(artist) {
-    this.setState({ artistQuery: artist });
-  }
-
-  handleSongInput(song) {
-    this.setState({ songQuery: song });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    if (this.state.artistQuery && this.state.songQuery) {
-      store.dispatch(searchLyrics(this.state.artistQuery, this.state.songQuery));
-    }
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    return (
-      <Lyrics
-        {...this.state}
-        handleChange={this.handleChange}
-        setArtist={this.handleArtistInput}
-        setSong={this.handleSongInput}
-        handleSubmit={this.handleSubmit} />
-    );
-  }
-
 }
+
+
+export default connect(mapStateToProps)(
+    class extends Component {
+
+    constructor() {
+
+      super();
+
+      this.state = {
+        artistQuery: '',
+        songQuery: ''
+      };
+
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleArtistInput = this.handleArtistInput.bind(this);
+      this.handleSongInput = this.handleSongInput.bind(this);
+
+    }
+
+    handleArtistInput(artist) {
+      this.setState({ artistQuery: artist });
+    }
+
+    handleSongInput(song) {
+      this.setState({ songQuery: song });
+    }
+
+    handleSubmit(e) {
+      e.preventDefault();
+      if (this.state.artistQuery && this.state.songQuery) {
+        store.dispatch(searchLyrics(this.state.artistQuery, this.state.songQuery));
+      }
+    }
+
+    render() {
+      return (
+        <Lyrics
+          {...this.props.state}
+          handleChange={this.handleChange}
+          setArtist={this.handleArtistInput}
+          setSong={this.handleSongInput}
+          handleSubmit={this.handleSubmit} />
+      );
+    }
+
+  }
+)
+
+// export default class extends Component {
+
+//   constructor() {
+
+//     super();
+
+//     this.state = Object.assign({
+//       artistQuery: '',
+//       songQuery: ''
+//     }, store.getState());
+
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.handleArtistInput = this.handleArtistInput.bind(this);
+//     this.handleSongInput = this.handleSongInput.bind(this);
+
+//   }
+
+//   componentDidMount() {
+//     this.unsubscribe = store.subscribe(() => {
+//       this.setState(store.getState());
+//     });
+//   }
+
+//   handleArtistInput(artist) {
+//     this.setState({ artistQuery: artist });
+//   }
+
+//   handleSongInput(song) {
+//     this.setState({ songQuery: song });
+//   }
+
+//   handleSubmit(e) {
+//     e.preventDefault();
+//     if (this.state.artistQuery && this.state.songQuery) {
+//       store.dispatch(searchLyrics(this.state.artistQuery, this.state.songQuery));
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     this.unsubscribe();
+//   }
+
+//   render() {
+//     return (
+//       <Lyrics
+//         {...this.state}
+//         handleChange={this.handleChange}
+//         setArtist={this.handleArtistInput}
+//         setSong={this.handleSongInput}
+//         handleSubmit={this.handleSubmit} />
+//     );
+//   }
+
+// }
--- browser/react/containers/PlayerContainer.js
+++ browser/react/containers/PlayerContainer.js
@@ -3,52 +3,101 @@ import AUDIO from '../audio';
 import store from '../store';
 import {previous, next, setProgress, toggleSong} from '../action-creators/player';
 import Player from '../components/Player';
+import { connect } from 'react-redux';
 
-export default class extends Component {
-
-  constructor() {
-    super();
-    this.state = store.getState().player;
-    this.toggle = this.toggle.bind(this);
+function mapStateToProps(state){
+  return {
+    state: state.player
   }
-
-  componentDidMount() {
-
-    AUDIO.addEventListener('ended', this.next);
-    AUDIO.addEventListener('timeupdate', () => {
-      store.dispatch(setProgress(AUDIO.currentTime / AUDIO.duration));
-    });
-
-    this.unsubscribe = store.subscribe(() => {
-      this.setState(store.getState().player);
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  next() {
-    store.dispatch(next());
-  }
-
-  prev() {
-    store.dispatch(previous());
-  }
-
-  toggle() {
-    store.dispatch(
-      toggleSong(this.state.currentSong, this.state.currentSongList)
-    );
-  }
-
-  render() {
-    return <Player
-      {...this.state}
-      next={this.next}
-      prev={this.prev}
-      toggle={this.toggle}
-    />;
-  }
-
 }
+
+function mapDispatchToProps(dispatch){
+  return {
+    next: function (){
+      dispatch(next())
+    },
+    prev: function(){
+      dispatch(previous())
+    },
+    toggle: function(){
+      dispatch(toggleSong(state.currentSong, state.currentSongList))
+    },
+    ProgressSetter: function(audio){
+      dispatch(setProgress(audio))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  class extends Component {
+
+    componentDidMount() {
+
+      AUDIO.addEventListener('ended', this.next);
+      AUDIO.addEventListener('timeupdate', () => {
+        this.props.ProgressSetter(AUDIO.currentTime / AUDIO.duration);
+      });
+    }
+
+    render() {
+      return <Player
+        {...this.props.state}
+        next={this.props.next}
+        prev={this.props.prev}
+        toggle={this.props.toggle}
+      />;
+    }
+
+  }
+)
+
+
+
+// export default class extends Component {
+
+//   constructor() {
+//     super();
+//     this.state = store.getState().player;
+//     this.toggle = this.toggle.bind(this);
+//   }
+
+//   componentDidMount() {
+
+//     AUDIO.addEventListener('ended', this.next);
+//     AUDIO.addEventListener('timeupdate', () => {
+//       store.dispatch(setProgress(AUDIO.currentTime / AUDIO.duration));
+//     });
+
+//     this.unsubscribe = store.subscribe(() => {
+//       this.setState(store.getState().player);
+//     });
+//   }
+
+//   componentWillUnmount() {
+//     this.unsubscribe();
+//   }
+
+//   next() {
+//     store.dispatch(next());
+//   }
+
+//   prev() {
+//     store.dispatch(previous());
+//   }
+
+//   toggle() {
+//     store.dispatch(
+//       toggleSong(this.state.currentSong, this.state.currentSongList)
+//     );
+//   }
+
+//   render() {
+//     return <Player
+//       {...this.state}
+//       next={this.next}
+//       prev={this.prev}
+//       toggle={this.toggle}
+//     />;
+//   }
+
+// }
